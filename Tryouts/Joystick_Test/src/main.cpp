c07e775688b269c7a7ee5a18e9cb55ac34103cd3
Greimelinio09 <felix.nzgleisdorf@gmail.com>
2026-06-06 10:47:42 +0200

    working motor speedcontroller

--- Tryouts/Joystick_Test/src/main.cpp
+++ Tryouts/Joystick_Test/src/main.cpp
@@ -3,24 +3,62 @@
 const int xPin = A0; // X-axis connected to analog pin A0
 const int yPin = A1; // Y-axis connected to analog pin A1
 const int buttonPin = 10; // Button connected to digital pin 10
+const int ledpins[] = {2, 3, 4}; // Array of LED pins
+
+int getspeed(int counter, int xValue);
+
 
 void setup() {
   Serial.begin(9600); // Start serial communication at 9600 baud rate
   pinMode(buttonPin, INPUT_PULLUP); // Set button pin as input with pull-up resistor
+  for (int i = 0; i < 3; i++) {
+    pinMode(ledpins[i], OUTPUT); // Set LED pins as outputs
+  }
 }
 
 void loop() {
+  
+  static int counter = 0;
+
   int xValue = analogRead(xPin); // Read the X-axis value
   int yValue = analogRead(yPin); // Read the Y-axis value
-  int buttonValue = digitalRead(buttonPin); // Read the button value
+  
+  counter = getspeed(counter,xValue);
+  analogWrite(ledpins[1], counter); // Set the brightness of the first LED based on the counter value
+  Serial.println(counter);
 
-  // Print the values to the Serial Monitor
-  Serial.print("X-axis: ");
-  Serial.print(xValue);
-  Serial.print(" | Y-axis: ");
-  Serial.println(yValue);
-  Serial.print("Button: ");
-  Serial.println(buttonValue == LOW ? "Pressed" : "Released");
+}
 
-  delay(500); // Delay for half a second before the next reading
+int getspeed(int counter, int xValue)
+{
+  const int checkvalue = 10; // Threshold value for adjusting brightness
+  static unsigned long lastchecktime = 0;
+ 
+  if (millis() - lastchecktime > 50) // Check every 100 milliseconds
+  {
+    lastchecktime = millis(); // Update the last check time
+    if(xValue > 700)
+      {
+        if(counter < 255-checkvalue)
+        {
+          counter += map(xValue, 700, 1023, 1, checkvalue); // Increase brightness
+        }
+        else
+        {
+          counter = 255; // Cap the brightness at 255
+        }
+      }
+      else if(xValue < 250)
+      {
+        if(counter > checkvalue)
+        {
+          counter -= map(xValue, 250, 0, 1, checkvalue); // Decrease brightness
+        }
+        else
+        {
+          counter = 0; // Cap the brightness at 0
+        }
+      }
+  }  
+    return counter;
 }
